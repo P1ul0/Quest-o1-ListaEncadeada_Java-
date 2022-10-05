a375b982a8ace55bf357961d768f1f3fe525371a
aluno <xfiller93@gmail.com>
2022-10-05 19:48:53 -0300

    att.

--- src/Entidade/Node.java
+++ src/Entidade/Node.java
@@ -5,22 +5,27 @@ public class Node  {
     private  Node prox;
 
     public Node(Site novo_Link) {
+
         this.valor = novo_Link;
     }
 
     public Site getValor() {
+
         return valor;
     }
 
     public void setValor(Site valor) {
+
         this.valor = valor;
     }
 
     public Node getProx() {
+
         return prox;
     }
 
     public void setProx(Node prox) {
+
         this.prox = prox;
     }
 }
